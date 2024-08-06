264040f074ddd6dbdc50c879b3437bb3722681d7
ZAID <mohammedzaid.connect@gmail.com>
2024-08-06 22:38:35 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -3,10 +3,8 @@ let mongoose=require("mongoose");
 let cors=require("cors");
 let bodyParser=require("body-parser");
 
-const employeeRoute = require("./routes/employeeroutes");
-const salaryRoute=require("./routes/salaryroutes")
 const newsRoute=require("./routes/newsroutes")
-mongoose.connect("mongodb://127.0.0.1:27017/Emp")
+mongoose.connect("mongodb://127.0.0.1:27017/News")
 .then((x)=>{
     console.log(`Connected to Mongo! Database name:"${x.connections[0].name}"`,);
 })
@@ -21,8 +19,6 @@ app.use(
     }),
 );
 app.use(cors());
-app.use("/employee",employeeRoute);
-app.use("/salary",salaryRoute)
 app.use("/news",newsRoute)
 
 const port=4000;
