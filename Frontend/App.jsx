13366f8d9efe9879e56496920ce942e44df04e90
ZAID <mohammedzaid.connect@gmail.com>
2024-08-08 03:13:07 -0100

    Update App.jsx

--- Frontend/App.jsx
+++ Frontend/App.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
-import Home from './pages/Home'
+import Home from './Components/pages/Home'
 import MenuBar from './Components/MenuBar'
-import CreateNews from './pages/CreateNews'
-import DailyNews from './pages/DailyNews'
-import { BrowserRouter as Routes, Route } from 'react-router-dom'
+import CreateNews from './Components/pages/CreateNews'
+import DailyNews from './Components/pages/DailyNews'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 function App() {
   return (
     <div>
       <MenuBar></MenuBar>
-      
+      <Router>
         <Routes>
           <Route path='/' element={<Home/>}></Route>
           <Route path='/create-news' element={<CreateNews/>}></Route>
           <Route path='/daily-news' element={<DailyNews/>}></Route>
         </Routes>
-      
+      </Router>
+    <Footer></Footer>
     </div>
   )
 }
 
 export default App
+
